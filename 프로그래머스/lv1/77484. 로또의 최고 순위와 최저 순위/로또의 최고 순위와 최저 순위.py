f8ff80d3813f0ead33a7abf66d1102931ca264d3
binary_ho <90160898+bainaryho@users.noreply.github.com>
2023-05-03 18:57:59 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,12 @@
+def solution(lottos, nums):
+    rank = {6:1,5:2,4:3,3:4,2:5,1:6,0:6} # 순위와 당첨 내용 딕셔너리
+    error = lottos.count(0) #낙서된 번호의 갯수(추후 최고순위에 +됨)
+    count = error
+    
+    for i in range(len(lottos)):
+        if lottos[i] in nums:
+            count += 1
+            
+    answer = [rank[count],rank[count-error]]
+    
+    return answer
