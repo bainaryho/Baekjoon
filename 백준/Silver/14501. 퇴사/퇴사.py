44864616d0623f98b0398066da469505a5b0edf5
binary_ho <90160898+bainaryho@users.noreply.github.com>
2023-07-30 20:02:31 +0900

    [Silver III] Title: 퇴사, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,22 @@
+import sys
+
+# 오늘로부터 N+1되는날퇴사
+# 남은 N일간 최대한 많은 상담
+
+n = int(input())  # 퇴사까지 남은 일수
+# 두가지를 배열로 만들어서 0으로 채워놓는다
+T = [0 for i in range(n + 1)]  # 상담을 완료하는데 걸리는 기간 T
+P = [0 for i in range(n + 1)]  # 상담으로 버는 돈 P
+# money[i]는 i번째 날까지 일 할경우 최대값
+money = [0 for i in range(n + 1)]
+
+for i in range(n):
+    T[i], P[i] = map(int, input().split())
+
+for i in range(len(T) - 2, -1, -1):  # 역순으로 진행
+    if T[i] + i <= n:  # 일수를 넘는지 확인
+        money[i] = max(P[i] + money[i + T[i]], money[i+1])
+    else:
+        money[i] = money[i+1]
+        
+print(money[0])
