17f8e5ff6829ae4f50b1e61a20a01cbbd6c8624c
binary_ho <90160898+bainaryho@users.noreply.github.com>
2024-05-28 07:55:51 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,13 +1,12 @@
-table = [list(map(int, input().split())) for _ in range(9)]
-
-max_num = -float('inf')
+max_num = -1
 max_row, max_col = 0, 0
 
 for i in range(9):
+    arr = list(map(int,input().split()))
     for j in range(9):
-        if table[i][j] > max_num:
-            max_num = table[i][j]
+        if arr[j] > max_num:
+            max_num = arr[j]
             max_row, max_col = i, j
 
 print(max_num)
-print(max_row + 1, max_col + 1)
+print(max_row+1, max_col+1)
