c04f33088270686d34df929afcd08eeb3f652bf7
binary_ho <90160898+bainaryho@users.noreply.github.com>
2024-09-10 19:09:42 +0900

    [Gold IV] Title: 전화번호 목록, Time: 4172 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,16 @@
+def is_consistent(phone_numbers):
+    phone_numbers.sort()
+    for i in range(len(phone_numbers) - 1):
+        if phone_numbers[i + 1].startswith(phone_numbers[i]):
+            return "NO"
+    return "YES"
+
+tests = int(input())
+for i in range(tests):
+    cases = int(input())
+    phone_numbers = []
+    for i in range(int(cases)):
+        phone_numbers.append(input())
+        
+    result = is_consistent(phone_numbers)
+    print(result)
